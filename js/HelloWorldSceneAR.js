8f6e64be08ab62204b252767b05ac97f706227bd
Tony Wong <wongtonyb@gmail.com>
2019-10-04 16:31:27 -0400

    first commit

---
+++ js/HelloWorldSceneAR.js
@@ -0,0 +1,69 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import { StyleSheet } from 'react-native';
+
+import {
+  ViroARScene,
+  ViroText,
+  ViroConstants,
+  ViroARPlaneSelector,
+  Viro3DObject,
+  ViroImage,
+  ViroNode,
+  ViroFlexView,
+  ViroARTrackingTargets,
+  ViroARImageMarker,
+  ViroBox,
+} from 'react-viro';
+
+export default class HelloWorldSceneAR extends Component {
+  constructor() {
+    super();
+
+    // Set initial state here
+    this.state = {
+      text: 'Initializing AR...',
+    };
+
+    // bind 'this' to functions
+    this._onInitialized = this._onInitialized.bind(this);
+  }
+
+  render() {
+    return (
+      <ViroARScene>
+        <ViroBox
+          position={[0, 0, -5]}
+          width={0.5}
+          height={0.5}
+          length={0.2}
+          scale={[1, 1, 1]}
+        />
+      </ViroARScene>
+    );
+  }
+
+  _onInitialized(state, reason) {
+    if (state == ViroConstants.TRACKING_NORMAL) {
+      this.setState({
+        text: 'Hello World!',
+      });
+    } else if (state == ViroConstants.TRACKING_NONE) {
+      // Handle loss of tracking
+    }
+  }
+}
+
+var styles = StyleSheet.create({
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+});
+
+module.exports = HelloWorldSceneAR;
